6858603270ebdb247a0ae471b66803d8d32fdef5
ramasamy <ramasamykr94@gmail.com>
2019-06-20 23:07:20 +0530

    Subraction part also added

--- Calculator/src/BasicMathOperation.java
+++ Calculator/src/BasicMathOperation.java
@@ -1,23 +1,47 @@
 import java.util.Scanner;
 
 public class BasicMathOperation {
-	
+
 	public static void main(String args[])
 	{
 		Scanner scan =new Scanner(System.in);
-		System.out.println("Enter the first number....");
-		int a = scan.nextInt();
-		System.out.println("Enter second number.......");
-		int b = scan.nextInt();
-		
-		int c = add(a,b);
-		
-		System.out.println("Result is ...."+c);
+		System.out.println("Enter the operation you want ADD OR SUB");
+		String operation = scan.nextLine();
+
+		switch (operation) {
+		case "ADD":
+			System.out.println("Enter the first number....");
+			int a = scan.nextInt();
+			System.out.println("Enter second number.......");
+			int b = scan.nextInt();
+			int c = add(a,b); 
+			System.out.println("Result is ...."+c);
+			break;
+		case "SUB":
+			System.out.println("Enter the first number....");
+			int d = scan.nextInt();
+			System.out.println("Enter second number.......");
+			int e = scan.nextInt();
+			int f = sub(d,e); 
+			System.out.println("Result is ...."+f);
+			break;
+		default:
+			System.out.println("Default Values are");
+			System.out.println("--------ADD-------");
+			System.out.println("--------SUB-------");
+			break;
+		}
+
 	}
-	
+
 	public static int add(int a ,int b)
 	{
 		int result = a+b;
+		return result;
+	}
+	public static int sub(int a ,int b)
+	{
+		int result = a-b;
 		return result;
 	}
 

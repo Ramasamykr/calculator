8939103687a3ade307cc92889c39f94ee7da1922
ramasamy <ramasamykr94@gmail.com>
2019-06-20 14:29:29 +0530

    Addition operation added

---
+++ Calculator/src/BasicMathOperation.java
@@ -0,0 +1,15 @@
+public class BasicMathOperation {
+	
+	public static void main(String args[])
+	{
+		int result = add(3,2);
+		System.out.println("result..."+result);
+	}
+	
+	public static int add(int a ,int b)
+	{
+		int result = a+b;
+		return result;
+	}
+
+}

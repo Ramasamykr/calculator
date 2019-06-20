d63fab7f9c2dc42bed71e87d90d04ed13b055712
ramasamy <ramasamykr94@gmail.com>
2019-06-20 22:50:07 +0530

    Getting values using command line

--- Calculator/src/BasicMathOperation.java
+++ Calculator/src/BasicMathOperation.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class BasicMathOperation {
 	
 	public static void main(String args[])
 	{
-		int result = add(3,2);
-		System.out.println("result..."+result);
+		Scanner scan =new Scanner(System.in);
+		System.out.println("Enter the first number....");
+		int a = scan.nextInt();
+		System.out.println("Enter second number.......");
+		int b = scan.nextInt();
+		
+		int c = add(a,b);
+		
+		System.out.println("Result is ...."+c);
 	}
 	
 	public static int add(int a ,int b)

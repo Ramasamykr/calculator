584405603436c15196e3037a16f18f0c5da8f3b8
ramasamy <ramasamykr94@gmail.com>
2019-06-21 11:12:05 +0530

    modulos operation added

--- Calculator/src/BasicMathOperation.java
+++ Calculator/src/BasicMathOperation.java
@@ -5,7 +5,7 @@ public class BasicMathOperation {
 	public static void main(String args[])
 	{
 		Scanner scan =new Scanner(System.in);
-		System.out.println("Enter the operation you want ADD OR SUB OR MUL OR DIV");
+		System.out.println("Enter the operation you want ADD OR SUB OR MUL OR DIV OR MOD");
 		String operation = scan.nextLine();
 
 		switch (operation) {
@@ -41,12 +41,21 @@ public class BasicMathOperation {
 			int l = div(j,k); 
 			System.out.println("Result is ...."+l);
 			break;
+		case "MOD":
+			System.out.println("Enter the first number....");
+			int m = scan.nextInt();
+			System.out.println("Enter second number.......");
+			int n = scan.nextInt();
+			int o = mod(m,n); 
+			System.out.println("Result is ...."+o);
+			break;	
 		default:
 			System.out.println("Default Values are");
 			System.out.println("--------ADD-------");
 			System.out.println("--------SUB-------");
 			System.out.println("--------MUL-------");
 			System.out.println("--------DIV-------");
+			System.out.println("--------MOD-------");
 			break;
 		}
 
@@ -70,6 +79,11 @@ public class BasicMathOperation {
 	public static int div(int a ,int b)
 	{
 		int result = a/b;
+		return result;
+	}
+	public static int mod(int a ,int b)
+	{
+		int result = a%b;
 		return result;
 	}
 
